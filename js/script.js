95b3c715a779d0b2b91696cc89f6ece3a646907b
Forklein <giuseppepisani2@gmail.com>
2021-07-26 16:49:36 +0200

    Add another function for print select option value

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const elementDisplay = document.getElementById('cards');
 let iconTemplate = '';
+
 //# Print cards Function
 const printCard = (arr, targetElement) => {
     arr.forEach((icon, index) => {
@@ -10,8 +11,8 @@ const printCard = (arr, targetElement) => {
         iconTemplate += `
         <div class="col-md-2 bg-white shadow p-3 border rounded-2 ${offsetDisplay} 
         d-flex flex-column align-items-center justify-content-center">
-           <i class="${icon.family} fa-${icon.name} fa-3x ${icon.type}"></i>
-           <p class="h4">${icon.name.toUpperCase()}</p>
+            <i class="${icon.family} fa-${icon.name} fa-3x ${icon.type}"></i>
+            <p class="h4">${icon.name.toUpperCase()}</p>
         </div>
         `;
     });
@@ -19,3 +20,21 @@ const printCard = (arr, targetElement) => {
 }
 
 printCard(icons, elementDisplay);
+
+//# Print option Function
+
+const filterDisplay = document.getElementById('filter');
+let optionTemplate = '<option>all</option>';
+
+const printOption = (arr, targetElement) => {
+    arr.forEach((icon) => {
+        if (!optionTemplate.includes(icon.type))
+            optionTemplate += `
+        <option>
+           ${icon.type}
+        </option>
+        `;
+    });
+    targetElement.innerHTML = optionTemplate;
+}
+printOption(icons, filterDisplay);
