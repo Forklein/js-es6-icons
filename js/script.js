a499fe58154200d084c54c9fe2a462a35d1fc936
Forklein <giuseppepisani2@gmail.com>
2021-07-26 16:12:21 +0200

    add arrow function and fix other

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 const elementDisplay = document.getElementById('cards');
-
-
-
-
 let iconTemplate = '';
-let offsetDisplay = '';
+//# Print cards Function
+const printCard = (arr, targetElement) => {
+    arr.forEach((icon, index) => {
+        let offsetDisplay = '';
+        if (index % 5 == 0) {
+            offsetDisplay = 'offset-md-1';
+        }
+        iconTemplate += `
+        <div class="col-md-2 bg-white shadow p-3 border rounded-2 ${offsetDisplay} 
+        d-flex flex-column align-items-center justify-content-center">
+           <i class="${icon.family} fa-${icon.name} fa-3x ${icon.type}"></i>
+           <p class="h4">${icon.name.toUpperCase()}</p>
+        </div>
+        `;
+    });
+    targetElement.innerHTML = iconTemplate;
+}
 
-icons.forEach((icon, index) => {
-    if (index % 5 == 0) {
-        offsetDisplay = 'offset-md-1';
-    } else {
-        offsetDisplay = '';
-    }
-    iconTemplate += `
-    <div class="col-md-2 bg-white shadow p-3 border rounded-2 ${offsetDisplay}">
-       <i class="${icon.fas} fa-${icon.type} fa-3x"></i>
-       <p class="h4">${icon.name}</p>
-    </div>
-    `;
-});
-elementDisplay.innerHTML = iconTemplate;
+printCard(icons, elementDisplay);
