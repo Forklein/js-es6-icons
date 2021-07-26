5eefc4cfe44cac560c02e8be5131503cb0b75393
Forklein <giuseppepisani2@gmail.com>
2021-07-26 17:29:47 +0200

    Final push

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 const elementDisplay = document.getElementById('cards');
-let iconTemplate = '';
 
 //# Print cards Function
 const printCard = (arr, targetElement) => {
+    let iconTemplate = '';
     arr.forEach((icon, index) => {
         let offsetDisplay = '';
         if (index % 5 == 0) {
             offsetDisplay = 'offset-md-1';
         }
         iconTemplate += `
-        <div class="col-md-2 bg-white shadow p-3 border rounded-2 ${offsetDisplay} 
+        <div class="col-md-2 ${offsetDisplay} 
         d-flex flex-column align-items-center justify-content-center">
-            <i class="${icon.family} fa-${icon.name} fa-3x ${icon.type}"></i>
-            <p class="h4">${icon.name.toUpperCase()}</p>
+            <div class="border rounded-2 bg-white shadow w-100 h-100 p-3">
+               <i class="${icon.family} fa-${icon.name} fa-3x ${icon.type}"></i>
+               <p class="h4">${icon.name.toUpperCase()}</p>
+            </div>
         </div>
         `;
     });
     targetElement.innerHTML = iconTemplate;
 }
-
 printCard(icons, elementDisplay);
 
 //# Print option Function
@@ -29,12 +30,24 @@ let optionTemplate = '<option>all</option>';
 const printOption = (arr, targetElement) => {
     arr.forEach((icon) => {
         if (!optionTemplate.includes(icon.type))
-            optionTemplate += `
-        <option>
-           ${icon.type}
-        </option>
-        `;
+            optionTemplate += `<option>${icon.type}</option>`;
     });
     targetElement.innerHTML = optionTemplate;
 }
 printOption(icons, filterDisplay);
+
+//# Filter
+
+
+filterDisplay.addEventListener('change', () => {
+    if (filterDisplay.value == 'all') {
+        printCard(icons, elementDisplay);
+        return;
+    }
+    const filteredIcons = icons.filter((icon) => {
+        if (icon.type == filterDisplay.value) {
+            return true;
+        }
+    });
+    printCard(filteredIcons, elementDisplay);
+});
