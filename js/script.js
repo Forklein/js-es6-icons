dd97ec0e9a1eb92c1d1744b33064471fa3ab213a
Forklein <giuseppepisani2@gmail.com>
2021-07-27 13:21:29 +0200

    Add search bar function

--- js/script.js
+++ js/script.js
@@ -23,7 +23,6 @@ const printCard = (arr, targetElement) => {
 printCard(icons, elementDisplay);
 
 //# Print option Function
-
 const filterDisplay = document.getElementById('filter');
 let optionTemplate = '<option>all</option>';
 
@@ -37,17 +36,36 @@ const printOption = (arr, targetElement) => {
 printOption(icons, filterDisplay);
 
 //# Filter
-
-
 filterDisplay.addEventListener('change', () => {
     if (filterDisplay.value == 'all') {
         printCard(icons, elementDisplay);
         return;
     }
-    const filteredIcons = icons.filter((icon) => {
-        if (icon.type == filterDisplay.value) {
-            return true;
+    const filteredIcons = icons.filter((icon) => icon.type == filterDisplay.value ? true : false);
+    printCard(filteredIcons, elementDisplay);
+    console.log(filteredIcons);
+});
+
+
+//# Take input from input value
+
+
+const displaySearch = document.getElementById('search');
+displaySearch.addEventListener('change', () => {
+    let elementInSearch = '';
+    icons.forEach((icon) => {
+        console.log(icon.name);
+        if (displaySearch.value.includes(icon.name)) {
+            console.log('FILTRATA', icon.name)
+            elementInSearch += `
+            <div class="d-flex flex-column align-items-center justify-content-center">
+                <div class="border rounded-2 bg-white shadow w-100 h-100 p-3">
+                   <i class="${icon.family} fa-${icon.name} fa-3x ${icon.type}"></i>
+                   <p class="h4">${icon.name.toUpperCase()}</p>
+                </div>
+            </div>
+           `
         }
     });
-    printCard(filteredIcons, elementDisplay);
+    elementDisplay.innerHTML = elementInSearch;
 });
